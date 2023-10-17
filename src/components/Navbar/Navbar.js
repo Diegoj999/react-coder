d8e18b5653c5176a0e584422fc7c8e0b04ef1028
Diego <diegoj9999@gmail.com>
2023-10-17 00:00:03 -0300

    Actualizacion completa v2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,10 +10,13 @@ const Navbar = () => {
   const { totalQuantity } = useContext(CartContext);
   const { user, profile, loading } = useAuth();
 
-  if (loading || !profile) return null;
+  if (loading) return;
 
   return (
-    <nav style={{background:"rgba(205, 205, 205, 0.212)"}} className="navbar navbar-expand-lg  ">
+    <nav
+      style={{ background: "rgba(205, 205, 205, 0.212)" }}
+      className="navbar navbar-expand-lg  "
+    >
       <div className="container-fluid mx-4">
         <Link to={"/"} className="navBar-logo">
           Ecommerce
@@ -47,23 +50,28 @@ const Navbar = () => {
               </Link>
             </li>
           </ul>
-          <div className="d-flex mx-3">
-            <NavLink to={"/cart"} className="text-decoration-none d-flex text-black">
-              <CardWidget totalQuantity={totalQuantity} />
-            </NavLink>
-            {!user ? (
-              <Link className="mx-2 text-decoration-none mt-3" to="/login">
-                Login
-              </Link>
-            ) : (
-              <img
-                src={profile.photoURL}
-                className="mx-2 navBar-img"
-                onClick={() => navigate(`/profile/`)}
-                alt={`Perfil de ${user.name}`}
-              ></img>
-            )}
-          </div>
+          {profile && (
+            <div className="d-flex mx-3">
+              <NavLink
+                to="/cart"
+                className="text-decoration-none d-flex text-black"
+              >
+                <CardWidget totalQuantity={totalQuantity} />
+              </NavLink>
+              {user ? (
+                <img
+                  src={profile?.photoURL}
+                  className="mx-2 navBar-img"
+                  onClick={() => navigate(`/profile/`)}
+                  alt={`Perfil de ${user.name}`}
+                />
+              ) : (
+                <Link className="mx-2 text-decoration-none mt-3" to="/login">
+                  Login
+                </Link>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </nav>
